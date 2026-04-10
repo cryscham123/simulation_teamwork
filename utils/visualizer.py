7954363a501c2c3d8148a35d23607bf4ce2257ad
cryscham123 <hyunghki@student.42seoul.kr>
2026-04-10 16:26:23 +0900

    기능 추가: machine에 대한 gantt chart를 완성하였다.
    bar 위에 글씨 보이게 하는건 길이가 짧은 막대도 존재하기 때문에 무리다.
    그냥 마우스 위에 올리면 보이니까 그렇게 봐라.

--- utils/visualizer.py
+++ utils/visualizer.py
@@ -3,7 +3,10 @@ import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from typing import List, Any
 
-def create_job_gantt_chart(jobs: List[Any], max_time:float, title: str = "Job Gantt Chart") -> go.Figure:
+def create_gantt_chart(logs: List[Any], 
+                           max_time:float, 
+                           title: str = "Job Gantt Chart",
+                           bar_margin: float = 0.3) -> go.Figure:
     """
     Job 리스트로부터 Gantt Chart를 생성
 
@@ -11,46 +14,38 @@ def create_job_gantt_chart(jobs: List[Any], max_time:float, title: str = "Job Ga
         jobs: Job 인스턴스 리스트
         max_time: 시뮬레이션 최대 시간
         title: 차트 제목
+        bar_margin: 간트 차트 bar 사이 간격
 
 
     Returns:
         Plotly Figure 객체
     """
     # 모든 Job의 이벤트 로그 수집
-    all_events = []
-    for job in jobs:
-        events = job.event_log
-        all_events.extend(events)
-    df_events = pd.DataFrame(all_events)
+    df_events = pd.DataFrame(logs)
 
     gantt_data = []
 
     pd.set_option('display.max_rows', None)  # 모든 행 출력
-    for job_id, job_events in df_events.groupby('job_id'):
-        for i, (_, event) in enumerate(job_events.sort_values('time').iterrows()):
-            if i != 0:
-                gantt_data[-1]['Finish'] = event['time'] if gantt_data[-1]['Resource'] not in ['interrupt', 'allocated'] else gantt_data[-1]['Start'] + 1
-                if gantt_data[-1]['Start'] == gantt_data[-1]['Finish']:
-                    gantt_data.pop()
-            if event['event_type'] == 'completed':
-                break
+    for id, events in df_events.groupby('id'):
+        for _, event in events.sort_values('start').iterrows():
+            if event['start'] == event['finish']:
+                continue
             gantt_data.append({
-                'Task': job_id,
-                'Start': event['time'],
-                'Resource': f"{event['event_type']}",
+                'Task': id,
+                'Start': event['start'],
+                'Finish': (event['finish'] if event['finish'] is not None else max_time) - bar_margin,
+                'Resource': f"{event['event']}",
                 'Description': f"{event['description']}"
             })
-        else:
-            gantt_data[-1]['Finish'] = max_time
 
     df_gantt = pd.DataFrame(gantt_data)
 
     colors = {
             "waiting": 'rgb(220, 220, 220)',  # 밝은 회색
-            "allocated": 'rgb(255, 195, 0)',  # 노란색
             "setup": 'rgb(0, 200, 83)', # 초록색
             "working": 'rgb(46, 137, 205)',  # 파란색
-            "interrupt": 'rgb(255, 65, 54)'  # 빨간색
+            "breakdown": 'rgb(255, 65, 54)',  # 빨간색
+            "repairing": 'rgb(255, 140, 0)'  # 주황색
     }
 
     fig = ff.create_gantt(
@@ -63,7 +58,7 @@ def create_job_gantt_chart(jobs: List[Any], max_time:float, title: str = "Job Ga
         showgrid_y=True,
         title=title,
     )
-    target_order = ["interrupt", "working", "setup", "allocated", "waiting"]
+    target_order = ["breakdown", "repairing", "working", "setup", "waiting"]
 
     fig.data = sorted(
         fig.data, 
@@ -76,7 +71,9 @@ def create_job_gantt_chart(jobs: List[Any], max_time:float, title: str = "Job Ga
         hovermode='closest',
         height=max(400, len(df_gantt['Task'].unique()) * 50),
         xaxis=dict(range=[0, max_time + 1], type='linear', dtick=5),
-        legend=dict(traceorder='reversed')
+        legend=dict(traceorder='reversed'),
+        bargap=1,
+        bargroupgap=1
     )
 
     return fig
