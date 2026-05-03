813015ea6c75bf20c5d76d57f307bf27b87fb15a
BBaekGGu <hun991006@gmail.com>
2026-05-04 02:48:50 +0900

    feat : 기존 rule들을 stocker에서 선택하는 rule로 변경했다.

--- simulation/stocker.py
+++ simulation/stocker.py
@@ -1,5 +1,7 @@
 from .job import Job
 import simpy
+import os
+import random
 
 class AllwaysTrueGroup():
     def __eq__(self, other):
@@ -38,15 +40,41 @@ class Stocker():
         yield self.__resource.put(job)
         job.prev_stocker = True
 
+    def __select_job(self, candidates, machine, rule):
+        """
+        JOB_RULE에 따라 stocker의 candidates 중 하나의 job을 선택
+        """
+        if rule == 'random':
+            return random.choice(candidates)
+        if rule == 'SPT':
+            return min(
+                candidates,
+                key=lambda j: machine.get_process_time(j.get_current_operation())
+            )
+        if rule == 'MIN_QTIME':
+            return min(candidates, key=lambda j: j.get_remain_qtime())
+        if rule == 'SPTSSU':
+            return min(
+                candidates,
+                key=lambda j: machine.get_setup_time(j.job_type)
+                + machine.get_process_time(j.get_current_operation())
+            )
+        raise ValueError(f"알 수 없는 JOB_RULE 값: {rule}")
+
     def wait_until_machine_ready(self):
         """
-        machine이 가용 가능한 상태가 되면 stocker에서 할당이 가능한 작업 한 개만 다시 machine을 할당 받을 수 있는 상태로 만들어줌
+        machine이 idle 신호를 보내면 JOB_RULE에 따라 stocker에서 job 한 개를 선택해 dispatch
         """
         while True:
             machine = yield self.machine_end_signal.get()
-            # stocker에 작업이 있으면 꺼내고, 아니면 그냥 패스
-            if len([x for x in self.__resource.items if x.get_op_group() == machine.group]) == 0:
+            candidates = [
+                x for x in self.__resource.items
+                if x.get_op_group() == machine.group
+            ]
+            if not candidates:
                 continue
-            job = yield self.__resource.get(lambda x: x.get_op_group() == machine.group)
+            rule = os.getenv('JOB_RULE', 'random')
+            best = self.__select_job(candidates, machine, rule)
+            job = yield self.__resource.get(lambda x: x is best)
             # job에 맞는 machine이 idle이 되면 다시 dispatch
             job.operation_end_signal.put(False)
